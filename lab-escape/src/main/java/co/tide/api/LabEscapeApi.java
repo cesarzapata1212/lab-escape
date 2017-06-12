c1a09b0b18337028b3dfe5dd450535d9893730dc
Cesar Zapata <czapata@uolinc.com>
2017-06-12 19:02:37 -0300

    Set up Spring Boot

---
+++ lab-escape/src/main/java/co/tide/api/LabEscapeApi.java
@@ -0,0 +1,22 @@
+package co.tide.api;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by czapata on 6/12/17.
+ */
+
+@RestController
+public class LabEscapeApi {
+
+
+    @RequestMapping(value = "/draw-escape", method = RequestMethod.POST)
+    public char[][] drawEscape(@RequestBody char[][] matrix){
+
+        return new char[0][0];
+    }
+
+}
---
+++ lab-escape/src/main/java/co/tide/app/LabEscapeApplication.java
@@ -0,0 +1,15 @@
+package co.tide.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by czapata on 6/12/17.
+ */
+
+@SpringBootApplication
+public class LabEscapeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LabEscapeApplication.class, args);
+    }
+}
